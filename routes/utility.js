52dd89ce98a09e4ca82976a30e707c8006d78273
Shazibali Gesawat <shazibali@icloud.com>
2018-05-28 06:31:04 +0530

    Update

--- routes/utility.js
+++ routes/utility.js
@@ -1,6 +1,7 @@
 //  One of our Endpoints of the group using express to model.
 const Bitcore = require("bitcore-lib");
 const Mnemonic = require("bitcore-mnemonic");
+const Request = require("request-promise");
  
 module.exports = (app) => {
 //  Generates mnemonic seeds (A paraphrase where each word is  converted into a numeric value)
@@ -11,7 +12,7 @@ module.exports = (app) => {
     });
 //  Getting the fiat value of the Bitcoin Balance 
     app.get("/balance/value", (request, response) => {
-        Request("https://api.coinmarketcap.com/v1/ticker/bitcoin/").then(market => {
+        Request('https://api.coinmarketcap.com/v1/ticker/bitcoin/').then(market => {
             response.send({ "value": "$" + (JSON.parse(market)[0].price_usd * request.query.balance).toFixed(2) });
         }, error => {
             response.status(500).send(error);
